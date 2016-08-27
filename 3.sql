ef7c75c4d3e0454c38a0bf4760554bec3c7509dd
Chris Barrett <c.barrett@me.com>
2016-08-27 13:44:22 -0500

    completed number 3

---
+++ 3.sql
@@ -0,0 +1,1 @@
+SELECT FirstName AS "First Name", LastName AS "Last Name", InvoiceId AS "ID", InvoiceDate AS "Date", BillingCountry AS "Country" FROM Customer JOIN Invoice ON Invoice.CustomerId == Customer.CustomerId WHERE Country = "Brazil"
