a4c768124202b5340d95b9d059038000e3e2eb76
Chris Barrett <c.barrett@me.com>
2016-08-28 17:27:53 -0500

    completed number 21

---
+++ 21.sql
@@ -0,0 +1,10 @@
+SELECT
+  Employee.FirstName || " " || Employee.LastName AS "Sales Agent",
+  SUM(Invoice.Total) AS "Total Sales"
+FROM Employee
+INNER JOIN Customer
+  ON Employee.EmployeeId = Customer.SupportRepId
+INNER JOIN Invoice
+  ON Customer.CustomerId = Invoice.CustomerId
+GROUP BY "Sales Agent"
+ORDER BY "Total Sales" DESC
