35695f1866f7e0da1dce429ef1c0e7043359cf49
Chris Barrett <c.barrett@me.com>
2016-08-29 17:39:36 -0500

    added line breaks

--- 3.sql
+++ 3.sql
@@ -1,1 +1,10 @@
-SELECT FirstName AS "First Name", LastName AS "Last Name", InvoiceId AS "ID", InvoiceDate AS "Date", BillingCountry AS "Country" FROM Customer JOIN Invoice ON Invoice.CustomerId == Customer.CustomerId WHERE Country = "Brazil"
+SELECT 
+  FirstName AS "First Name", 
+  LastName AS "Last Name", 
+  InvoiceId AS "ID", 
+  InvoiceDate AS "Date", 
+  BillingCountry AS "Country" 
+FROM Customer 
+JOIN Invoice 
+  ON Invoice.CustomerId == Customer.CustomerId 
+WHERE Country = "Brazil"
